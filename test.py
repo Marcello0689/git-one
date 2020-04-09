b7dbb58714caea487f41caa269659239fccb8153
Marcello <marcello.s.1989@gmail.com>
2020-04-09 22:54:45 +0200

    Piccola modifica

--- test.py
+++ test.py
@@ -6,11 +6,9 @@ print (os.getcwd())
 
 conn = sqlite3.connect('SQLite_Python.db')
 c = conn.cursor()
-
-
 identificativo = 0
 for n in range (10000):
-	value = random.uniform(0.498, 0.503)
+	value = random.uniform(0.495, 0.509)
 	identificativo = identificativo + 1
 	x = (identificativo, value)
 	try:
@@ -19,15 +17,27 @@ for n in range (10000):
 		print('Insert succesful')
 	except sqlite3.Error as e:
 		print('An error occurred:' , e.args[0])
-conn.close()
+
+
+identificativo = 0
+for m in range (10000):
+	value = random.uniform(0.498, 0.503)
+	identificativo = identificativo + 1
+	x = (identificativo, value)
+	try:
+		c.execute('INSERT INTO table2 VALUES (?,?)', x)
+		conn.commit()
+		print('Insert succesful')
+	except sqlite3.Error as e:
+		print('An error occurred:' , e.args[0])
 	
 identificativo = 0
-for n in range (10000):
+for t in range (10000):
 	value = random.uniform(0.499, 0.501)
 	identificativo = identificativo + 1
 	x = (identificativo, value)
 	try:
-		c.execute('INSERT INTO table1 VALUES (?,?)', x)
+		c.execute('INSERT INTO table3 VALUES (?,?)', x)
 		conn.commit()
 		print('Insert succesful')
 	except sqlite3.Error as e:
