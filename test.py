56878343690ce319f2fb6741088f4701167e1de2
Marcello <marcello.s.1989@gmail.com>
2020-04-09 22:53:18 +0200

    Modificata la chiusura del database, non si può chiudere e poi chiedere di continuare la scrittura

--- test.py
+++ test.py
@@ -6,21 +6,8 @@ print (os.getcwd())
 
 conn = sqlite3.connect('SQLite_Python.db')
 c = conn.cursor()
-identificativo = 0
-for n in range (10000):
-	value = random.uniform(0.495, 0.509)
-	identificativo = identificativo + 1
-	x = (identificativo, value)
-	try:
-		c.execute('INSERT INTO table1 VALUES (?,?)', x)
-		conn.commit()
-		print('Insert succesful')
-	except sqlite3.Error as e:
-		print('An error occurred:' , e.args[0])
-conn.close()
 
-conn = sqlite3.connect('SQLite_Python.db')
-c = conn.cursor()
+
 identificativo = 0
 for n in range (10000):
 	value = random.uniform(0.498, 0.503)
@@ -34,8 +21,6 @@ for n in range (10000):
 		print('An error occurred:' , e.args[0])
 conn.close()
 	
-conn = sqlite3.connect('SQLite_Python.db')
-c = conn.cursor()
 identificativo = 0
 for n in range (10000):
 	value = random.uniform(0.499, 0.501)
